0bc523f4be54c439aac2c13b1576cd18aaf1503d
38096 <iminib39@gmail.com>
2019-08-19 21:21:26 +0300

    Added script db

--- src/main/java/com/example/DrivingService/DemoApplication.java
+++ src/main/java/com/example/DrivingService/DemoApplication.java
@@ -2,8 +2,10 @@ package com.example.DrivingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
 public class DemoApplication {
 
 	public static void main(String[] args) {
